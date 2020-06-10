3c004e13b057f5f58abf5848f23ba5987a02f688
June21x <35992041+June21x@users.noreply.github.com>
2020-06-11 01:32:28 +0800

    Navigation
    -Added navigation buttons for all scene
    -Linked all scenes with navgation buttons

---
+++ Perfect Potion/Assets/Navigation.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Navigation : MonoBehaviour
+{
+    public void OpenMainMenu()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    public void OpenPotionShopScene()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void OpenFarmScene()
+    {
+        SceneManager.LoadScene(2);
+    }
+
+    public void OpenCraftScene()
+    {
+        SceneManager.LoadScene(3);
+    }
+
+    public void OpenInventoryScene()
+    {
+        SceneManager.LoadScene(4);
+    }
+    public void OpenTradeScene()
+    {
+        SceneManager.LoadScene(5);
+    }
+    public void OpenRecipeScene()
+    {
+        SceneManager.LoadScene(6);
+    }
+    public void ToggleMoreNavigation(GameObject gameObject)
+    {
+        gameObject.SetActive(!gameObject.activeSelf);
+    }
+
+}
